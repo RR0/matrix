61a99997faa5109ed5341ed5e79f246120566b00
javarome <javarome@gmail.com>
2017-07-14 17:52:48 +0200

    ES6 version

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Known phenomena matrix
 
+See [Description](http://rr0.org/time/1/9/7/7/Poher_Matrice/).
+
 See it in action on [CodePen](https://codepen.io/Javarome/pen/xrBOrr).
--- matrix.js
+++ matrix.js
@@ -1,266 +1,247 @@
-angular
-  .module("rr0-matrix", [])
-  .service("matrixService", [
-    "$log",
-    "$q",
-    "$rootScope",
-    "$http",
-    function ($log, $q, $rootScope, $http) {
-      "use strict";
-      function Question(t, i) {
-        this.title = t;
-        this.choices = {};
-        this.index = i;
-      }
+class Question {
+  constructor(t, i) {
+    this.title = t;
+    this.choices = {};
+    this.index = i;
+  }
+}
 
-      var self = this;
+class Choice {
+  constructor(l, a, knownPhenomenaProbabilities) {
+    this.label = l;
+    this.answerType = a;
+    this.knownPhenomenaProbabilities = knownPhenomenaProbabilities;
+    this.value = false;
+  }
+}
 
-      function Choice(l, a, knownPhenomenaProbabilities) {
-        this.label = l;
-        this.answerType = a;
-        this.knownPhenomenaProbabilities = knownPhenomenaProbabilities;
-        this.value = false;
-      }
+class MatrixService {
 
-      function loadURL(u) {
-        return $q((resolve, reject) = > {
-            $http({method: "GET", url: u})
-            .then((data, status, headers, config) = > {
-            $log.info("Loaded '" + u + "'");
-        resolve(data.data);
-      })
-      .
-        catch((data, status, headers, config) = > {
-          reject("Could not load '" +u + "': " + status
-      )
-        ;
-      })
-        ;
-      })
-        ;
-      }
+  constructor($log, $q, $rootScope, $http) {
+    this.$log = $log;
+    this.$q = $q;
+    this.$rootScope = $rootScope;
+    this.$http = $http;
+  }
 
-      function loadFile(f) {
-        return $q((resolve, reject) = > {
-            var matrixFileReader = new FileReader();
-        matrixFileReader.onloadend = e =
-      >
-        {
-          try {
-            var matrixData = JSON.parse(e.target.result);
-            resolve(matrixData);
-          } catch (err) {
-            reject(err);
-          }
+  loadURL(u) {
+    return this.$q((resolve, reject) => {
+      this.$http({method: "GET", url: u})
+        .then(response => {
+          this.$log.info("Loaded '" + u + "'");
+          resolve(response.data);
+        })
+        .catch((response, status) => {
+          reject("Could not load '" + u + "': " + status);
+        });
+    });
+  }
+
+  loadFile(f) {
+    return this.$q((resolve, reject) => {
+      const matrixFileReader = new FileReader();
+      matrixFileReader.onloadend = e => {
+        try {
+          const matrixData = JSON.parse(e.target.result);
+          resolve(matrixData);
+        } catch (err) {
+          reject(err);
         }
-        ;
-        matrixFileReader.readAsText(f);
-      })
-        ;
-      }
+      };
+      matrixFileReader.readAsText(f);
+    });
+  }
 
-      function loadLabels(labelsInput) {
-        return typeof labelsInput === "object"
-          ? loadFile(labelsInput)
-          : loadURL(labelsInput);
-      }
+  loadLabels(labelsInput) {
+    return typeof labelsInput === "object"
+      ? this.loadFile(labelsInput)
+      : this.loadURL(labelsInput);
+  }
 
-      function loadMatrixData(matrixData, labelsInput) {
-        loadLabels(labelsInput).then(
-          messages = > {
-          msg = messages;
-        var i = 0;
-        var questions = (self.questions = {});
-        for (var d in matrixData) {
-          if (matrixData.hasOwnProperty(d)) {
-            var item = matrixData[d];
-            var dotPos = item.question.indexOf(".");
-            var questionKey = item.question.substring(0, dotPos);
-            var choiceKey = item.question.substring(dotPos + 1);
-            var question = questions[questionKey];
-            if (!question) {
-              question = new Question(messages[questionKey], i++);
-              questions[questionKey] = question;
+  loadMatrixData(matrixData, labelsInput) {
+    this.loadLabels(labelsInput)
+      .then(messages => {
+          this.msg = messages;
+          let i = 0;
+          const questions = (this.questions = {});
+          for (const d in matrixData) {
+            if (matrixData.hasOwnProperty(d)) {
+              const item = matrixData[d];
+              const dotPos = item.question.indexOf(".");
+              const questionKey = item.question.substring(0, dotPos);
+              const choiceKey = item.question.substring(dotPos + 1);
+              let question = questions[questionKey];
+              if (!question) {
+                question = new Question(messages[questionKey], i++);
+                questions[questionKey] = question;
+              }
+              question.choices[choiceKey] = new Choice(
+                messages[item.question],
+                item.answertype,
+                item.knownPhenomenaProbabilities
+              );
             }
-            question.choices[choiceKey] = new Choice(
-              messages[item.question],
-              item.answertype,
-              item.knownPhenomenaProbabilities
-            );
           }
-        }
-        $rootScope.$broadcast("dataLoaded", questions);
-      },
-        function (reason) {
-          $log.error(reason);
-          $rootScope.$broadcast("dataError", reason);
-        }
-
-      )
-        ;
-      }
-
-      var msg;
-      return {
-        load: function (matrixInput, labelsInput) {
-          if (typeof matrixInput === "object") {
-            loadFile(matrixInput)
-              .then(matrixData = > loadMatrixData(matrixData, labelsInput)
-          )
-          .
-            catch(reason = > $rootScope.$broadcast("dataError", reason)
-          )
-            ;
-          } else {
-            loadURL(matrixInput)
-              .then(matrixData = > loadMatrixData(matrixData, labelsInput)
-          )
-          .
-            catch(reason = > $rootScope.$broadcast("dataError", reason)
-          )
-            ;
-          }
+          this.$rootScope.$broadcast("dataLoaded", questions);
         },
-        compute: function (probable) {
-          var zerosCount = {};
-          var max = 0;
-          var questions = self.questions;
-          for (var q in questions) {
-            if (questions.hasOwnProperty(q)) {
-              var question = questions[q];
-              for (var c in question.choices) {
-                if (question.choices.hasOwnProperty(c)) {
-                  var choice = question.choices[c];
-                  if (choice.value !== false) {
-                    var knownPhenomenaProbabilities =
-                      choice.knownPhenomenaProbabilities;
-                    for (var p in knownPhenomenaProbabilities) {
-                      if (knownPhenomenaProbabilities.hasOwnProperty(p)) {
-                        if (!zerosCount[p]) {
-                          zerosCount[p] = 0;
-                        }
-                        if (knownPhenomenaProbabilities[p] === 0) {
-                          zerosCount[p]++;
-                          if (zerosCount[p] > max) {
-                            max = zerosCount[p];
-                          }
-                        }
-                      }
+        function (reason) {
+          this.$log.error(reason);
+          this.$rootScope.$broadcast("dataError", reason);
+        }
+      );
+  }
+
+  load(matrixInput, labelsInput) {
+    if (typeof matrixInput === "object") {
+      this.loadFile(matrixInput)
+        .then(matrixData => this.loadMatrixData(matrixData, labelsInput))
+        .catch(reason => this.$rootScope.$broadcast("dataError", reason));
+    } else {
+      this.loadURL(matrixInput)
+        .then(matrixData => this.loadMatrixData(matrixData, labelsInput))
+        .catch(reason => this.$rootScope.$broadcast("dataError", reason));
+    }
+  }
+
+  compute(probable) {
+    const zerosCount = {};
+    let max = 0;
+    const questions = this.questions;
+    for (const q in questions) {
+      if (questions.hasOwnProperty(q)) {
+        const question = questions[q];
+        for (const c in question.choices) {
+          if (question.choices.hasOwnProperty(c)) {
+            const choice = question.choices[c];
+            if (choice.value !== false) {
+              const knownPhenomenaProbabilities = choice.knownPhenomenaProbabilities;
+              for (const p in knownPhenomenaProbabilities) {
+                if (knownPhenomenaProbabilities.hasOwnProperty(p)) {
+                  if (!zerosCount[p]) {
+                    zerosCount[p] = 0;
+                  }
+                  if (knownPhenomenaProbabilities[p] === 0) {
+                    zerosCount[p]++;
+                    if (zerosCount[p] > max) {
+                      max = zerosCount[p];
                     }
                   }
                 }
               }
             }
           }
-          var explanations = [];
-          for (var z in zerosCount) {
-            if (zerosCount.hasOwnProperty(z)) {
-              var count = zerosCount[z];
-              var index = probable ? max - count : count;
-              var trad = msg[z];
-              if (explanations[index]) {
-                explanations[index] += ", " + trad;
-              } else {
-                explanations[index] =
-                  trad.charAt(0).toUpperCase() + trad.slice(1);
-              }
-            }
-          }
-          var explanationsWithoutHoles = [];
-          for (var i = 0; i < explanations.length; i++) {
-            if (explanations[i]) {
-              explanationsWithoutHoles.push(explanations[i]);
-            }
-          }
-          return explanationsWithoutHoles;
-        }
-      };
-    }
-  ])
-  .controller("MatrixFormController", [
-    "$log",
-    "$scope",
-    "matrixService",
-    function ($log, $scope, matrixService) {
-      "use strict";
-      $scope.load = function () {
-        var matrixInput =
-          document.getElementById("matrixFile").files[0] || $scope.matrixURL;
-        var labelsInput =
-          document.getElementById("labelsFile").files[0] || $scope.labelsURL;
-        if (matrixInput && labelsInput) {
-          $scope.questions = [];
-          matrixService.load(matrixInput, labelsInput);
-        }
-      };
-
-      $scope.questionIndex = 0;
-
-      $scope.parameterChanged = function () {
-        $scope.questionIndex = $scope.currentQuestion.index;
-        questionChanged();
-      };
-      function questionChanged() {
-        $scope.currentQuestion =
-          $scope.questions[$scope.questionsKeys[$scope.questionIndex]];
-        function Field(type, label) {
-          this.type = type;
-          this.label = label;
-        }
-
-        $scope.fields = {};
-        var choices = $scope.currentQuestion.choices;
-        var keys = Object.keys(choices);
-        for (var i = 0; i < keys.length; i++) {
-          var key = keys[i];
-          $scope.fields[key] = new Field(
-            choices[key].answerType,
-            choices[key].label
-          );
         }
       }
-
-      $scope.$on("dataError", function (event, msg) {
-        $log.error(msg);
-        window.alert(msg);
-      });
-
-      $scope.$on("dataLoaded", function (event, questions) {
-        $scope.questions = questions;
-        $scope.questionsKeys = Object.keys(questions);
-        questionChanged();
-      });
-
-      $scope.onPrevious = function () {
-        if ($scope.questionIndex >= 0) {
-          $scope.questionIndex--;
-          questionChanged();
+    }
+    const explanations = [];
+    for (const z in zerosCount) {
+      if (zerosCount.hasOwnProperty(z)) {
+        const count = zerosCount[z];
+        const index = probable ? max - count : count;
+        const trad = this.msg[z];
+        if (explanations[index]) {
+          explanations[index] += ", " + trad;
+        } else {
+          explanations[index] =
+            trad.charAt(0).toUpperCase() + trad.slice(1);
         }
-      };
+      }
+    }
+    const explanationsWithoutHoles = [];
+    for (let i = 0; i < explanations.length; i++) {
+      if (explanations[i]) {
+        explanationsWithoutHoles.push(explanations[i]);
+      }
+    }
+    return explanationsWithoutHoles;
+  }
+}
 
-      $scope.onNext = function () {
-        if ($scope.questionIndex < $scope.questionsKeys.length) {
-          $scope.questionIndex++;
-          questionChanged();
-        }
-      };
+class Field {
+  constructor(type, label) {
+    this.type = type;
+    this.label = label;
+  }
+}
+class MatrixFormController {
+  constructor($log, $scope, matrixService) {
+    this.$log = $log;
+    this.$scope = $scope;
+    this.matrixService = matrixService;
 
-      $scope.resultsType = "NonProbable";
-      $scope.compute = function (key) {
-        var changedChoice = $scope.currentQuestion.choices[key];
-        if (changedChoice && changedChoice.answerType === "radio") {
-          for (var c in $scope.currentQuestion.choices) {
-            if ($scope.currentQuestion.choices.hasOwnProperty(c)) {
-              var choice = $scope.currentQuestion.choices[c];
-              if (choice.answerType === "radio") {
-                choice.value = c === key ? key : false;
-              }
-            }
+    this.resultsType = "NonProbable";
+    this.questionIndex = 0;
+
+    $scope.$on("dataError", (event, msg) => {
+      $log.error(msg);
+      window.alert(msg);
+    });
+
+    $scope.$on("dataLoaded", (event, questions) => {
+      this.questions = questions;
+      this.questionsKeys = Object.keys(questions);
+      this.questionChanged();
+    });
+  }
+
+  load() {
+    const matrixInput = document.getElementById("matrixFile").files[0] || this.matrixURL;
+    const labelsInput = document.getElementById("labelsFile").files[0] || this.labelsURL;
+    if (matrixInput && labelsInput) {
+      this.questions = [];
+      this.matrixService.load(matrixInput, labelsInput);
+    }
+  }
+
+  parameterChanged() {
+    this.questionIndex = this.currentQuestion.index;
+    this.questionChanged();
+  }
+
+  questionChanged() {
+    this.currentQuestion = this.questions[this.questionsKeys[this.questionIndex]];
+
+    this.fields = {};
+    const choices = this.currentQuestion.choices;
+    const keys = Object.keys(choices);
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      this.fields[key] = new Field(
+        choices[key].answerType,
+        choices[key].label
+      );
+    }
+  }
+
+  onPrevious() {
+    if (this.questionIndex >= 0) {
+      this.questionIndex--;
+      this.questionChanged();
+    }
+  }
+
+  onNext() {
+    if (this.questionIndex < this.questionsKeys.length) {
+      this.questionIndex++;
+      this.questionChanged();
+    }
+  }
+
+  compute(key) {
+    const changedChoice = this.currentQuestion.choices[key];
+    if (changedChoice && changedChoice.answerType === "radio") {
+      for (const c in this.currentQuestion.choices) {
+        if (this.currentQuestion.choices.hasOwnProperty(c)) {
+          const choice = this.currentQuestion.choices[c];
+          if (choice.answerType === "radio") {
+            choice.value = c === key ? key : false;
           }
         }
-        $scope.explanations = matrixService.compute(
-          $scope.resultsType === "NonProbable"
-        );
-      };
+      }
     }
-  ]);
+    this.explanations = this.matrixService.compute(this.resultsType === "NonProbable");
+  }
+}
+angular.module("rr0-matrix", [])
+  .service("matrixService", MatrixService)
+  .controller("MatrixFormController", MatrixFormController);
